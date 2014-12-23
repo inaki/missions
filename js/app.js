f343efd64df1f7cbc5ddbb403c685cfe702aced1
Imanol Aranzadi <iiaranzadi@gmail.com>
2014-12-22 20:07:01 -0500

    small slider at the head using ui.bootstrap, and hex tiles from json file."

--- js/app.js
+++ js/app.js
@@ -5,12 +5,71 @@ angular.module('missionsApp', ['ui.bootstrap'])
     {
       title: 'hola mundo!',
       text: 'lorem ipsum ikirulakd',
-      image: 'http://placekitten.com/g/1200/250'
+      image: 'http://placekitten.com/1200/200'
     },{
       title: 'Otro gatito!',
       text: 'lorem ipsum ikirulakd',
-      image: 'http://placekitten.com/g/1200/250'
+      image: 'http://placekitten.com/1200/200'
     }
+    ];
+
+    $scope.missions = [
+      {
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'codeforamerica'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'codeforde'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'oaklandjs'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'nodebots'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'jsgirls'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'nodeschool'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'git-it'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'csvconf'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'koa'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'promise'
+      },{
+        name: 'Trasnportation Data',
+        intro: 'Data about transportation',
+        description: 'lorem lakjfd ajf klaj fkja fj adfkadjflkj adlfkj adlkjf aldkjflakj flkajflkja f jadflkjadfkj aj fajdf ',
+        image: 'binary'
+      }
     ];
 
   }]);
